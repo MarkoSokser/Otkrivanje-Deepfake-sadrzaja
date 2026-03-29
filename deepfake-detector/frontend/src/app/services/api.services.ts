709ee918577c84ddfaf7270ef5485b812b6255c0
msokser22 <msokser22@student,foi.hr>
2026-03-29 15:54:02 +0200

    feat:ReadME

---
+++ deepfake-detector/frontend/src/app/services/api.services.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://127.0.0.1:8000';
+
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(`${this.apiUrl}/analyze`, formData);
+  }
+}
