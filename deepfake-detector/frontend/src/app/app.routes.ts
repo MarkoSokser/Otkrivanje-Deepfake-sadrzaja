750a93ab3010eb08f7cda7ae4cdbfda8b7f05fd4
msokser22 <msokser22@student,foi.hr>
2026-04-03 20:51:52 +0200

    feat: rutes

--- deepfake-detector/frontend/src/app/app.routes.ts
+++ deepfake-detector/frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { AnalyzeComponent } from './components/analyze/analyze';
 import { ResultsComponent } from './components/results/results';
+import { HistoryComponent } from './components/history/history';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'analyze', component: AnalyzeComponent },
   { path: 'results', component: ResultsComponent },
+  { path: 'history', component: HistoryComponent },
   { path: '**', redirectTo: '' },
 ];
--- deepfake-detector/frontend/src/app/app.ts
+++ deepfake-detector/frontend/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
+import { ToastComponent } from './components/toast/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
